7618474c0ffc9bde7421dfb376b1c03e790258e9
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-06-03 12:05:07 +0900

    Add files via upload

---
+++ smile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*draw smile*/
+
+void main()
+{
+	printf("\t¡á\t¡á\n\n");
+	printf("  ¡á\t\t     ¡á\n");
+	printf("   ¡á\t\t    ¡á\n");
+	printf("     ¡á¡á¡á¡á¡á¡á¡á¡á");
+}
