1cc09d1f3cd067a05999122ad82867b5d6841f55
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-06-06 15:22:25 +0900

    Add files via upload

---
+++ c project/별 그리기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*make star*/
+
+void main()
+{
+	printf("       *\n");
+	printf("      * *\n");
+	printf("     *   *\n");
+	printf("*****     *****\n");
+	printf(" *           *\n");
+	printf("  *         *\n");
+	printf("   *       *\n"); 
+	printf("  *    *    *\n");
+	printf(" *  *     *  *\n");
+	printf("* *          * *\n");
+} 
---
+++ c project/스마일 그리기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*draw smile*/
+
+void main()
+{
+	printf("\t¡á¡á\t¡á¡á\n\t¡á¡á\t¡á¡á\n");
+	printf("  ¡á¡á\t\t     ¡á¡á\n");
+	printf("   ¡á¡á\t\t    ¡á¡á\n");
+	printf("     ¡á¡á¡á¡á¡á¡á¡á¡á¡á");
+}
