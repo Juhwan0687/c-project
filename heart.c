7811401ff50f79fb4d45caafa934214bf079acc0
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-06-06 15:05:27 +0900

    Add files via upload

---
+++ heart.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*하트 그리기*/
+
+void main()
+{
+	printf("##\n");
+	printf("  ♡♡      ♡♡\n");
+	printf("♡♡♡♡  ♡♡♡♡\n");
+	printf(" ♡♡♡♡♡♡♡♡\n");
+	printf("  ♡♡♡♡♡♡♡\n");
+	printf("   ♡♡♡♡♡♡\n");
+	printf("     ♡♡♡♡\n");
+	printf("      ♡♡♡\n");
+	printf("        ♡\n");
+}
