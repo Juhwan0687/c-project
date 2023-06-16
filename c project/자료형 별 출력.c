d5b93fa98e0829ef025a62fbc08fb95ae5c64a3c
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-06-16 16:27:14 +0900

    Add files via upload

---
+++ c project/자료형 별 출력.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	int a;
+	double f1;
+	char ch;
+	
+	a=15;
+	f1=123.456;
+	ch='A';
+	printf("%d\n",a);
+	printf("%.3lf\n",f1);
+	printf("%c\n",ch);
+}
