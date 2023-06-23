461f64c4212eacc1f9e1106cd72c32ea384c634a
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-06-23 16:36:29 +0900

    Add files via upload

---
+++ c project/사칙연산.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//자료형 테스트하기 
+int main(){
+	printf("%d\n",10/3);
+	printf("%d\n",10/3.0);
+	printf("%lf",10/3.0);
+	return 0;
+}
+/*사칙연산 
+void main(){
+	int a;
+	int b;
+	double c;
+	double d;
+	scanf("%d %d",&a,&b);
+	printf("%d\n",a+b);
+	printf("%d\n\n",a/b);
+	scanf("%lf %lf",&c,&d);
+	printf("%lf\n",c+d);
+	printf("%lf",c/d);
+}
+*/
