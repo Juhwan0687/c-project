ab721f7159f8032da301f88ef283703122cc4d20
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-06-24 10:36:37 +0900

    Add files via upload

---
+++ c project/자료형 연산2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	int a=34/5;
+	double b=34/5.0;
+	double c=34.0/5;
+	double d=34.0/5.0; 
+	printf("%d\n",a);
+	printf("%lf\n",b);
+	printf("%lf\n",c);
+	printf("%lf\n",d);
+}
