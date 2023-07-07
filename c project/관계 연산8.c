2104a2ea043cff7d18dbeb2ccd46439da2c81faa
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-07-07 16:27:30 +0900

    Create 관계 연산8.c

---
+++ c project/관계 연산8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int a=20;
+	
+	a=a+10;
+	printf("a=%d\n",a);
+	a=a*10;
+	printf("a=%d\n",a);
+	a=a-10;
+	printf("a=%d\n",a);
+	a=a/10;
+	printf("a=%d\n",a);
+	return 0;
+}
