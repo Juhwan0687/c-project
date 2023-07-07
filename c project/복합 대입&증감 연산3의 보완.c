48dedceee0d2312a84ca46c0c02378819e3ce6cf
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-07-07 17:21:12 +0900

    Create 복합 대입&증감 연산3의 보완.c

---
+++ c project/복합 대입&증감 연산3의 보완.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a=12;
+	int b=3;
+	a-=b++;
+	printf("a=%d b=%d\n",a,b);
+	a/=--b;
+	printf("a=%d b=%d\n",a,b);
+	a+=b++;
+	printf("a=%d b=%d\n",a,b);
+	a-=++b;
+	printf("a=%d b=%d\n",a,b);
+	a=b--;
+	printf("a=%d b=%d",a,b);
+}
