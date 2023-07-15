c1e80798b9793a0f15432e8da825b3c2701fdfc8
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-07-15 10:17:01 +0900

    Create 큰 실수 찾기.c

---
+++ c project/큰 실수 찾기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	double f1,f2;
+	
+	f1=0.000001;
+	f2=0.000002;
+	
+	if(f1<f2)
+	{
+		printf("留욎븘!!!\n");
+	}
+	else
+	{
+		printf("?꾨땲??!!\n");
+	}
+		return 0;
+}
