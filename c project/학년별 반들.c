6264c409774d077a8de3b3231f35966dc32e5fc1
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-08-18 16:10:47 +0900

    Add files via upload

---
+++ c project/학년별 반들.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int i,j;
+	for(i=1;i<=6;i++){
+		printf("%dÇÐ³â ",i);
+		for(j=1;j<=7;j++){
+			printf("%d¹Ý ",j);
+		}
+		printf("\n");
+	}	
+}
