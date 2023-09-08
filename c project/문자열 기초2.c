34a0fdef1ddd4dae4c5a909c09617ea76b40c3f3
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-09-08 16:43:50 +0900

    Create 문자열 기초2.c

---
+++ c project/문자열 기초2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	char str1[15]={'H','i',' ','C','3',' ','C','o','d','i','n','g'};
+	char str2[15]={'H','i',' ','C','3',' ','C','o','d','i','n','g','\0'};
+	
+	printf("%s\n",str1);
+	printf("%s\n",str2);
+}
