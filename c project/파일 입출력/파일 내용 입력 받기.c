1153d8ce941b7e74191b84c393bd52de59adf0f8
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-10-20 17:30:12 +0900

    Add files via upload

---
+++ c project/파일 입출력/파일 내용 입력 받기.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	FILE *in=fopen("NumberFile.txt","r");
+	int n,m;
+	double d1,d2;
+	
+	if(in==NULL){
+		printf("파일 없음\n");
+		exit(1);
+	}
+	
+	printf("정수를 두 개 입력합니다\n");
+	fscanf(in,"%d %d",&n,&m);
+	printf("실수를 두 개 입력합니다\n");
+	fscanf(in,"%lf %lf",&d1,&d2);
+	
+	printf("\n입력 받은 수들을 모두 출력합니다.\n");
+	printf("%d %d\n",n,m);
+	printf("%lf %lf\n",d1,d2);
+	printf("\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+	printf("어디서 입력 받았을까요?\n");
+
+	fclose(in);
+}
